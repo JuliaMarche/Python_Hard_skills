17158fa22b992b52a50893548b43ba9a05a2563c
JuliaMarche <yuliamarche@gmail.com>
2022-06-23 13:53:37 +0300

    Разбиение действий на методы

--- test_add_group.py
+++ test_add_group.py
@@ -3,10 +3,10 @@ from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.common.exceptions import NoSuchElementException
 from selenium.common.exceptions import NoAlertPresentException
-import unittest, time, re
+import unittest, time
 
 options = Options()
-options.binary_location = r'C:\Program Files\Mozilla Firefox\firefox.exe'
+options.binary_location = r'C:\Users\marchenko.js\AppData\Local\Mozilla Firefox\firefox.exe'
 
 class test_add_group(unittest.TestCase):
     def setUp(self):
@@ -15,19 +15,27 @@ class test_add_group(unittest.TestCase):
 
     def test_test_add_group(self):
         wd = self.wd
-        wd.get("http://localhost/addressbook/")
-        wd.find_element_by_name("user").click()
-        wd.find_element_by_name("user").clear()
-        wd.find_element_by_name("user").send_keys("admin")
-        wd.find_element_by_name("pass").click()
-        wd.find_element_by_name("pass").clear()
-        wd.find_element_by_name("pass").send_keys("secret")
+        self.open_home_page(wd)
+        self.login(wd)
+        self.open_groups_page(wd)
+        self.init_group_creation(wd)
+        self.fill_group_form(wd)
+        self.submit_group_creation(wd)
+        self.return_to_groups_page(wd)
+        self.logout(wd)
+
+    def logout(self, wd):
+        wd.find_element_by_link_text("Logout").click()
+
+    def return_to_groups_page(self, wd):
+        wd.find_element_by_link_text("group page").click()
         time.sleep(1)
-        wd.find_element_by_xpath("//input[@value='Login']").click()
-        time.sleep(2)
-        wd.find_element_by_id("container").click()
-        wd.find_element_by_link_text("groups").click()
-        wd.find_element_by_name("new").click()
+
+    def submit_group_creation(self, wd):
+        wd.find_element_by_name("submit").click()
+        time.sleep(1)
+
+    def fill_group_form(self, wd):
         wd.find_element_by_name("group_name").click()
         wd.find_element_by_name("group_name").clear()
         wd.find_element_by_name("group_name").send_keys("test")
@@ -40,11 +48,27 @@ class test_add_group(unittest.TestCase):
         wd.find_element_by_name("group_footer").clear()
         wd.find_element_by_name("group_footer").send_keys("testtest")
         time.sleep(1)
-        wd.find_element_by_name("submit").click()
+
+    def init_group_creation(self, wd):
+        wd.find_element_by_name("new").click()
+
+    def open_groups_page(self, wd):
+        wd.find_element_by_id("container").click()
+        wd.find_element_by_link_text("groups").click()
+
+    def login(self, wd):
+        wd.find_element_by_name("user").click()
+        wd.find_element_by_name("user").clear()
+        wd.find_element_by_name("user").send_keys("admin")
+        wd.find_element_by_name("pass").click()
+        wd.find_element_by_name("pass").clear()
+        wd.find_element_by_name("pass").send_keys("secret")
         time.sleep(1)
-        wd.find_element_by_link_text("group page").click()
-        time.sleep(1)
-        wd.find_element_by_link_text("Logout").click()
+        wd.find_element_by_xpath("//input[@value='Login']").click()
+        time.sleep(2)
+
+    def open_home_page(self, wd):
+        wd.get("http://localhost/addressbook/")
 
     def is_element_present(self, how, what):
         try:
