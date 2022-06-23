eeb37a5259a679f6f25d6b74f85a6f88b4391134
Julia Marchenko <marchenko.js@kraud.ru>
2022-06-23 20:28:59 +0300

    Добавление вспомогательного класса для теста контактов

--- test_add_group.py
+++ test_add_group.py
@@ -6,6 +6,7 @@ from selenium.common.exceptions import NoAlertPresentException
 from selenium.webdriver.support.select import Select
 import unittest, time
 from group import Group
+from contact import Contact
 
 options = Options()
 options.binary_location = r'C:\Program Files\Mozilla Firefox\firefox.exe'
@@ -31,11 +32,11 @@ class test_add_group(unittest.TestCase):
         self.open_home_page(wd)
         self.login(wd, username="admin", password="secret")
         self.init_contact_creation(wd)
-        self.fill_contact_form(wd)
+        self.fill_contact_form(wd, Contact(firstname="Test", middlename="Test", lastname="Test", nickname="Test", title="Test", company="Test", address="Test", home="Test", work="Test", email="Test", bday="19", bmonth="June", byear="1994", address2="Test", notes="Test"))
         self.submit_contact_creation(wd)
         time.sleep(1)
         self.return_to_home_page(wd)
-        time.sleep(1)
+        time.sleep(2)
         self.logout(wd)
 
 
@@ -45,52 +46,50 @@ class test_add_group(unittest.TestCase):
     def submit_contact_creation(self, wd):
         wd.find_element_by_xpath("//div[@id='content']/form/input[21]").click()
 
-    def fill_contact_form(self, wd):
+    def fill_contact_form(self, wd, contact):
         # Add firstname
         wd.find_element_by_name("firstname").click()
-        wd.find_element_by_name("firstname").send_keys("Test")
+        wd.find_element_by_name("firstname").send_keys(contact.firstname)
         # Add middlename
         wd.find_element_by_name("middlename").click()
-        wd.find_element_by_name("middlename").send_keys("Test")
+        wd.find_element_by_name("middlename").send_keys(contact.middlename)
         # Add lastname
         wd.find_element_by_name("lastname").click()
-        wd.find_element_by_name("lastname").send_keys("Test")
+        wd.find_element_by_name("lastname").send_keys(contact.lastname)
         # Add nickname
         wd.find_element_by_name("nickname").click()
-        wd.find_element_by_name("nickname").send_keys("Test")
+        wd.find_element_by_name("nickname").send_keys(contact.nickname)
         # Add title
         wd.find_element_by_name("title").click()
-        wd.find_element_by_name("title").send_keys("Test test")
+        wd.find_element_by_name("title").send_keys(contact.title)
         # Add company
         wd.find_element_by_name("company").click()
-        wd.find_element_by_name("company").send_keys("Test")
+        wd.find_element_by_name("company").send_keys(contact.company)
         # Add address
         wd.find_element_by_name("address").click()
-        wd.find_element_by_name("address").send_keys("Test test test")
+        wd.find_element_by_name("address").send_keys(contact.address)
         # Add home
         wd.find_element_by_name("home").click()
-        wd.find_element_by_name("home").send_keys("Test")
+        wd.find_element_by_name("home").send_keys(contact.home)
         # Add work
         wd.find_element_by_name("work").click()
-        wd.find_element_by_name("work").send_keys("Test")
+        wd.find_element_by_name("work").send_keys(contact.work)
         # Add email
         wd.find_element_by_name("email").click()
-        wd.find_element_by_name("email").send_keys("test@test.test")
+        wd.find_element_by_name("email").send_keys(contact.email)
         # Choose birthday
         wd.find_element_by_name("bday").click()
-        Select(wd.find_element_by_name("bmonth")).select_by_visible_text("June")
-        wd.find_element_by_xpath("//option[@value='19']").click()
+        Select(wd.find_element_by_name("bday")).select_by_visible_text(contact.bday)
         wd.find_element_by_name("bmonth").click()
-        Select(wd.find_element_by_name("bmonth")).select_by_visible_text("June")
-        wd.find_element_by_xpath("//option[@value='June']").click()
+        Select(wd.find_element_by_name("bmonth")).select_by_visible_text(contact.bmonth)
         wd.find_element_by_name("byear").click()
-        wd.find_element_by_name("byear").send_keys("1994")
+        wd.find_element_by_name("byear").send_keys(contact.byear)
         # Add address
         wd.find_element_by_name("address2").click()
-        wd.find_element_by_name("address2").send_keys("test")
+        wd.find_element_by_name("address2").send_keys(contact.address2)
         # Add notes
         wd.find_element_by_name("notes").click()
-        wd.find_element_by_name("notes").send_keys("comment")
+        wd.find_element_by_name("notes").send_keys(contact.notes)
 
     def init_contact_creation(self, wd):
         wd.find_element_by_link_text("add new").click()
