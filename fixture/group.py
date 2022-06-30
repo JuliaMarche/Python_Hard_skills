d384944db6453dbd2d98546cfbcc30ac4e3fd515
JuliaMarche <yuliamarche@gmail.com>
2022-06-30 11:36:23 +0300

    Создание автотеста для удаления группы

--- fixture/group.py
+++ fixture/group.py
@@ -24,6 +24,15 @@ class GroupHelper:
         self.submit_group_creation()
         self.return_to_groups_page()
 
+    def delete_first_group(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        # select first group
+        wd.find_element_by_name("selected[]").click()
+        # submit deletion
+        wd.find_element_by_name("delete").click()
+        self.return_to_groups_page()
+
     def init_group_creation(self):
         wd = self.app.wd
         wd.find_element_by_name("new").click()
