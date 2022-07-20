1644b17adbbe56270668e8af8f0bb0754d44061f
JuliaMarche <yuliamarche@gmail.com>
2022-07-20 16:55:50 +0300

    Сравнение списков групп

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -2,6 +2,9 @@ from model.group import Group
 
 def test_add_group(app):
     old_groups = app.group.get_group_list()
-    app.group.fill_form(Group(name="test", header="test", footer="testtest"))
+    group = Group(name="test", header="test", footer="testtest")
+    app.group.fill_form(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(group)
+    assert old_groups == new_groups
