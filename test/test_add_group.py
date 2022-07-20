0c2a702a749f566a4beb78530a53e72fbb231d9a
JuliaMarche <yuliamarche@gmail.com>
2022-07-20 11:58:57 +0300

    Загрузка списков групп

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -1,4 +1,7 @@
 from model.group import Group
 
 def test_add_group(app):
+    old_groups = app.group.get_group_list()
     app.group.fill_form(Group(name="test", header="test", footer="testtest"))
+    new_groups = app.group.get_group_list()
+    assert len(old_groups) + 1 == len(new_groups)
