94444fbe327d241052f3751dc15a5905f46d49e3
Julia Marchenko <marchenko.js@kraud.ru>
2022-07-26 20:25:43 +0300

    Тесты для проверки контактов

--- test/test_communication_methods.py
+++ test/test_communication_methods.py
@@ -21,7 +21,7 @@ def marge_phones_like_on_home_page(contact):
     return "\n".join(filter(lambda x: x != "",
                             map(lambda x: clear(x),
                                 filter(lambda x: x is not None,
-                                       [contact.homephone, contact.mobile, contact.workphone]))))
+                                       [contact.homephone, contact.mobile, contact.workphone, contact.secondphone]))))
 
 def merge_emails_like_on_home_page(contact):
     return "\n".join(filter(lambda x: x != "",
