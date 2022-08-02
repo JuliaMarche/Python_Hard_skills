020ec247ad3dd39931926767e277d080212984bc
JuliaMarche <yuliamarche@gmail.com>
2022-08-02 16:10:18 +0300

    Динамическая генерация тестов

---
+++ data/contacts.py
@@ -0,0 +1,10 @@
+from model.contact import Contact
+
+testdata = [
+    Contact(firstname="Test", middlename="Test", lastname="Test", nickname="Test", title="Test", company="Test",
+          address="Test", homephone="6666666666666", workphone="55555555555555", mobile="77777777", secondphone="9999999", email="Test",
+          second_email="Test", third_email="Test", bday="19", bmonth="June", byear="1994", second_address="Test", notes="Test"),
+    Contact(firstname="Test2", middlename="Test2", lastname="Test2", nickname="Test2", title="Test2", company="Test2",
+          address="Test2", homephone="6666666666666", workphone="55555555555555", mobile="77777777", secondphone="9999999", email="Test2",
+          second_email="Test2", third_email="Test2", bday="18", bmonth="June", byear="1992", second_address="Test2", notes="Test2")
+]
